25fa7423710eb93737770a04b0d14e00298da4df
James de Lisle <james.delisle@riskaware.co.uk>
2020-04-22 19:56:03 +0100

    ref #8 Added the stubs for the data storage module.

---
+++ data/writer.py
@@ -0,0 +1,11 @@
+
+
+class Writer:
+
+    @staticmethod
+    def write_to_json(array, path, filename):
+        pass
+
+    @staticmethod
+    def write_to_pickle(array, path, filename):
+        pass
