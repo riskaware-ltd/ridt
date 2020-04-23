0c69942fcde8ac3aff6fffdbc205497d3875d607
James de Lisle <james.delisle@riskaware.co.uk>
2020-04-23 20:11:08 +0100

    ref #12 Added some stub methods to the Basic class.

--- runcontainer/basic.py
+++ runcontainer/basic.py
@@ -1,5 +1,22 @@
+import numpy as np
+
+from config.idmfconfig import IDMFConfig
 
 
 class Basic:
 
-    pass
+    def __init__(self, settings: IDMFConfig):
+
+        self.settings = settings
+        self.__build_parameter_space()
+    
+    def __build_parameter_space(self):
+        self._X, self._Y, self._Z, self._T = np.meshgrid(X, Y, Z, T, indexing='ij')
+    
+    @property
+    def space(self):
+        return self._X, self._Y, self._Z, self._T
+    
+    def evaluate_over_parameter_space(self, function):
+        pass
+
