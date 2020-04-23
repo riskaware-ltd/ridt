3c63735e23f5a0559609ae8aea3fb0c2d457683b
James de Lisle <james.delisle@riskaware.co.uk>
2020-04-23 20:16:31 +0100

    ref #12 Added a settings property to the Basic class.

--- runcontainer/basic.py
+++ runcontainer/basic.py
@@ -7,16 +7,19 @@ class Basic:
 
     def __init__(self, settings: IDMFConfig):
 
-        self.settings = settings
+        self._settings = settings
         self.__build_parameter_space()
     
     def __build_parameter_space(self):
         self._X, self._Y, self._Z, self._T = np.meshgrid(X, Y, Z, T, indexing='ij')
     
     @property
+    def settings(self):
+        return self._settings
+
+    @property
     def space(self):
         return self._X, self._Y, self._Z, self._T
     
     def evaluate_over_parameter_space(self, function):
         pass
-
