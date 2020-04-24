3f5b100be7a2238c55436648f867b041202c4d30
James de Lisle <james.delisle@riskaware.co.uk>
2020-04-24 22:33:06 +0100

    ref #16 Removed test.py.

--- test.py
+++
@@ -1,9 +0,0 @@
-import json
-from config.idmfconfig import IDMFConfig
-
-with open('default/config.json', 'r') as f:
-    d = json.loads(f.read())
-
-t = IDMFConfig(d)
-
-print(t.total_air_change_rate)
