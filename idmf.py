b3277c2ed9090fc63820649ddd3e0c5539cfee35
James de Lisle <james.delisle@riskaware.co.uk>
2020-04-25 09:52:42 +0100

    ref #18 Added configfileparser to cli for testing

--- idmf.py
+++ idmf.py
@@ -1,9 +1,14 @@
 import click
 
+from config.configfileparser import ConfigFileParser
 
 @click.command()
-@click.argument('config_file', type=click.Path(exists=True), help='The path to the configuration file.', nargs=-1)
-@click.argument('output_dir', type=click.Path(exists=True), help='The path to the output directory.', nargs=1)
+@click.argument('config_file', type=click.Path(exists=True), nargs=-1)
+@click.argument('output_dir', type=click.Path(exists=True), nargs=1)
 def idmf(config_file, output_dir):
     """Run one or several config files and write all data to the given directory."""
-    pass
+    with ConfigFileParser() as cfp:
+        f = cfp(config_file)
+
+if __name__ == '__main__':
+    idmf()
