8475c1ac9f63a6ebbb6791afc6b1c6ca4a29480b
Isaac-JenkinsRA <isaac.jenkins@riskaware.co.uk>
2020-04-28 10:39:08 +0100

    refs 13 Bug Fixes and Structural Changes.
    Changed the release type models to have the single underscore instead of the double as getattr() could not find the private method. Fixed a bug in fixed release where the concentration was exponentially increasing when no sources were emitting. Removed c_end and t_end as we already know that c_end is the source.end_time and we can work out c_end from that index value.

--- equation/well_mixed.py
+++ equation/well_mixed.py
@@ -1,4 +1,3 @@
-from config.ConfigFileParser import ConfigFileParser
 from config.idmfconfig import IDMFConfig
 
 import numpy as np
@@ -10,21 +9,12 @@ class WellMixed:
         pass
 
     def __call__(self, settings: IDMFConfig, time_array: np.ndarray):
-        """try:
-            return getattr(self, f"__{self.settings.release_type}")(settings, time_array)
-        except AttributeError as e:
-            raise e"""
+        try:
+            return getattr(self, f"_{settings.release_type}")(settings, time_array)
+        except AttributeError:
+            f"Release type must be instantaneous, fixed_duration or infinite_duration"
 
-        if settings.release_type == "instantaneous":
-            return self.__instantaneous(settings, time_array)
-
-        if settings.release_type == "fixed_duration":
-            return self.__fixed(settings, time_array)
-
-        if settings.release_type == "infinite_duration":
-            return self.__infinite(settings, time_array)
-
-    def __instantaneous(self, settings: IDMFConfig, time_array: np.ndarray):
+    def _instantaneous(self, settings: IDMFConfig, time_array: np.ndarray):
         sources = settings.modes.instantaneous.sources
         concentration = []
         for source_name, source in sources.items():
@@ -47,7 +37,7 @@ class WellMixed:
                 )
         return concentration
 
-    def __infinite(self, settings: IDMFConfig, time_array: np.ndarray):
+    def _infinite_duration(self, settings: IDMFConfig, time_array: np.ndarray):
         sources = settings.modes.infinite_duration.sources
         concentration = []
         for source_name, source in sources.items():
@@ -70,7 +60,7 @@ class WellMixed:
                 )
         return concentration
 
-    def __fixed(self, settings: IDMFConfig, time_array: np.ndarray):
+    def _fixed_duration(self, settings: IDMFConfig, time_array: np.ndarray):
         sources = settings.modes.fixed_duration.sources
         concentration = []
         for source_name, source in sources.items():
@@ -83,7 +73,7 @@ class WellMixed:
         volume = settings.models.well_mixed.volume
         fa_rate = settings.fresh_air_change_rate
         concentration = []
-        for time in time_array:
+        for index, time in enumerate(time_array):
             if time < source.start_time:
                 concentration.append(0)
             if source.start_time <= time <= source.end_time:
@@ -92,28 +82,9 @@ class WellMixed:
                         -(fa_rate / volume) * (time - source.start_time)))
                 )
             if time > source.end_time:
-                if concentration[-1] >= concentration[-2]:
-                    c_end = concentration[-1]
-                    end_index = concentration.index(c_end)
-                    t_end = time_array[end_index]
                 concentration.append(
-                    c_end * np.exp(
-                        -(fa_rate / volume) * ((time - source.start_time) - t_end)
+                    concentration[int(source.end_time) - 1] * np.exp(
+                        -(fa_rate / volume) * (time - source.end_time)
                     )
                 )
         return concentration
-
-
-c = ConfigFileParser()
-config = c("../default/config.json")
-
-w = WellMixed()
-y = w(config, np.array(range(100)))
-
-import matplotlib.pyplot as plt
-
-plt.plot(range(0, 100, 1), y)
-plt.xlabel("Time")
-plt.ylabel("Concentration")
-plt.title("Well Mixed Instantaneous release with two sources.")
-plt.show()
