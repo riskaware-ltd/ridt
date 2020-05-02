69d121df5129c66c2efdba9bd5b9b8e46453be5f
James de Lisle <james.delisle@riskaware.co.uk>
2020-05-02 15:02:02 +0100

    ref #21 basic structure layed out for wellmixedrun

--- container/wellmixedrun.py
+++ container/wellmixedrun.py
@@ -1,6 +1,6 @@
-import numpy as np
-
-from pprint import pprint
+from numpy import zeros
+from numpy import meshgrid
+from numpy import linspace
 
 from config.idmfconfig import IDMFConfig
 from base.settings import ComputationalSpace
@@ -14,31 +14,26 @@ class WellMixedRun:
         self.prepare()
         self.build_parameter_space()
     
-    def prepare(self):
-        getattr(self, f"prepare_{self.settings.release_type}")()
-
     def build_parameter_space(self):
-        self._space = np.linspace(0.0, self.settings.total_time, self.settings.time_samples)
+        self._time= linspace(0.0,
+                             self.settings.total_time,
+                             self.settings.time_samples)
+        self._space = (self.settings.time_samples)
     
     def prepare(self):
-
         restrict = { 
             "modes": self.settings.release_type,
             "models": self.settings.dispersion_model
         }
 
         space = ComputationalSpace(self.settings, restrict)
-        print(space.shape)
-        space.axes
+        data = zeros(shape=(*space.shape, *self._space))
+   
    
     @property
     def settings(self):
         return self._settings
 
     @property
-    def space(self):
-        return self._space
-    
-    def evaluate_over_parameter_space(self, function):
-        pass
-
+    def time(self):
+        return self._time
