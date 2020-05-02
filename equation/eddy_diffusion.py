5b373fafdd5cdf6f9786b94660d4fc42e7b61bfd
James de Lisle <james.delisle@riskaware.co.uk>
2020-05-02 10:29:39 +0100

    ref #14 Working implementation of EddyDiffusion
    The current configuration is a working eddy diffusion model for all run
    types.

--- equation/eddy_diffusion.py
+++ equation/eddy_diffusion.py
@@ -1,4 +1,5 @@
 from numpy import ndarray
+from numpy import array
 from numpy import zeros
 from numpy import ones
 from numpy import full
@@ -9,6 +10,10 @@ from numpy import heaviside
 from numpy import maximum
 from numpy import where
 from numpy import cumsum
+from numpy import clip
+
+from typing import List
+from copy import copy
 
 from config.configfileparser import ConfigFileParser
 from config.idmfconfig import IDMFConfig
@@ -21,77 +26,76 @@ class EddyDiffusion:
         self.settings = settings
         self.dim = self.settings.models.eddy_diffusion.dimensions
         self.volume = self.dim.x * self.dim.y * self.dim.z
-        self.shape = (self.settings.time_samples,
-                      self.settings.spatial_samples,
+        self.shape = (self.settings.spatial_samples,
                       self.settings.spatial_samples,
                       self.settings.spatial_samples)
-        self.conc = zeros(self.shape)
         self.zero = zeros(self.shape)
-        self.one = ones(self.shape)
+        self.conc = [copy(self.zero) for i in range(self.settings.time_samples)]
+        self.delta_t = self.settings.total_time / self.settings.time_samples
         self.diff_coeff = self.__diffusion_coefficient()
+        self.sources = getattr(self.settings.modes, self.settings.release_type).sources
 
-    def __call__(self, x: float, y: float,  z: float, t: float):
+    def __call__(self, x: ndarray, y: ndarray,  z: ndarray, t: ndarray):
         try:
-            return  getattr(self, f"_{self.settings.release_type}")(x, y, z, t)
+            return  getattr(self, f"{self.settings.release_type}")(x, y, z, t)
         except AttributeError as e:
             f"Release type must be instantaneous, fixed_duration or infinite_duration"
 
-    def _instantaneous(self, x: float, y: float, z: float, t: float):
-        sources = self.settings.modes.instantaneous.sources
-        for source in sources.values():
-            t_shift = maximum(t - source.time, self.one)
-            self.conc += self.heaviside(t, source.time) *\
-                source.mass * self.__concentration(source, x, y, z, t_shift)
-        return self.conc
+    def instantaneous(self, x: ndarray, y: ndarray, z: ndarray, t: List[float]): 
+        for idx, time in enumerate(t):
+            for source in self.sources.values():
+                if time - source.time > 0:
+                    self.conc[idx] += source.mass * self.__concentration(
+                        source, x, y, z, time - source.time)
+        return array(self.conc)
 
-    def _infinite_duration(self, x: float, y: float, z: float, t: float):
-        sources = getattr(self.settings.modes, self.settings.release_type).sources
-        for source in sources.values():
-            t_shift = maximum(t - source.time, self.one)
-            self.conc +=  self.heaviside(t, source.time) *\
-                source.rate * self.__concentration(source, x, y, z, t_shift)
-        return cumsum(self.conc, axis=0) *\
-            (self.settings.total_time / self.settings.time_samples)
+    def infinite_duration(self, x: ndarray, y: ndarray, z: ndarray, t: List[float]):
+        for idx, time in enumerate(t):
+            for source in self.sources.values():
+                if time - source.time > 0:
+                    self.conc[idx] += source.rate * self.__concentration(
+                        source, x, y, z, time - source.time)
+        self.conc = array(self.conc)
+        return cumsum(self.conc, axis=0) * self.delta_t
     
-    def _fixed_duration(self, x: float, y: float, z: float, t: float):
-        sources = getattr(self.settings.modes, self.settings.release_type).sources
-        for source in sources.values():
-            print(self.conc.shape)
-            t_shift = maximum(t - source.start_time, self.one)
-            self.conc += self.heaviside(t, source.start_time) *\
-                source.rate * self.__concentration(source, x, y, z, t_shift) -\
-                self.heaviside(t, source.end_time) *\
-                source.rate * self.__concentration(source, x, y, z, t_shift)
-        return cumsum(self.conc, axis=0) *\
-            (self.settings.total_time / self.settings.time_samples)
+    def fixed_duration(self, x: ndarray, y: ndarray, z: ndarray, t: List[float]):
+        self.conc_decay = [copy(self.zero) for i in range(self.settings.time_samples)]
+        for idx, time in enumerate(t):
+            for source in self.sources.values():
+                if time - source.start_time > 0:
+                    self.conc[idx] += source.rate * self.__concentration(
+                        source, x, y, z, time - source.start_time)
+                if time - source.start_time - source.end_time > 0:
+                    self.conc_decay[idx] += source.rate * self.__concentration(
+                        source, x, y, z, time - source.start_time - source.end_time)
+        self.conc = cumsum(array(self.conc), axis=0) * self.delta_t
+        self.conc_decay = cumsum(array(self.conc_decay), axis=0) * self.delta_t
+        return self.conc - self.conc_decay
     
-    def heaviside(self, value: float, offset: float = 0):
-        return where(value > offset, self.one, self.zero)
-
     def __concentration(self,
                        source: InstantaneousSource,
-                       x: float,
-                       y: float,
-                       z: float,
+                       x: ndarray,
+                       y: ndarray,
+                       z: ndarray,
                        t: float):
         r_x = self.__exp(x, t, self.dim.x, source.x)
         r_y = self.__exp(y, t, self.dim.y, source.y)
         r_z = self.__exp(z, t, self.dim.z, source.z)
         return self.__coeff(t) * r_x * r_y * r_z
 
-    def __exp(self, position: float, t: float, bound: float, source_loc: float):
+    def __exp(self, position: ndarray, t: float, bound: float, source_loc: float):
 
         image_num = self.settings.models.eddy_diffusion.images.quantity
         def image(arg):
-            return exp((-power(arg, 2)) / 4 * self.diff_coeff * t)
+            return exp(-power(arg, 2) / (4 * self.diff_coeff * t))
 
         rv = []
         for image_index in range(-image_num, image_num + 1):
-            rv.append(image(position + 2 * image_index * bound - source_loc) +
-                      image(position + 2 * image_index * bound + source_loc))
+            value = position + 2 * image_index * bound
+            rv.append(image(value - source_loc) + image(value + source_loc))
         return sum(rv)
 
-    def __coeff(self, t: float):
+    def __coeff(self, t: ndarray):
         fa_rate = self.settings.fresh_air_change_rate
         num = exp(-t * fa_rate / self.volume)
         den  = 8 * power(pi * self.diff_coeff * t, 3 / 2)
