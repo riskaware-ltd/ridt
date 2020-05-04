64cbc3fd551e50197fba2a81a8102464da4fb84d
James de Lisle <james.delisle@riskaware.co.uk>
2020-05-04 16:02:40 +0100

    ref #9 added lineplot/contourplot remove plot

---
+++ analysis/lineplot.py
@@ -0,0 +1,11 @@
+
+
+class LinePlot:
+
+    def __init__(self, settings: IDMFConfig):
+        pass
+
+    def __call__(self, xvalues: List[float], concvalues: List[float], line: LineSetting, outputpath: str):
+        plot = self.plot(xvalues, concvalues, line)
+        
+        plt.savefig(fdjksalfdkl;afd)
