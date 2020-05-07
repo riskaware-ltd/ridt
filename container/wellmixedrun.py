74175130d1bdacd3140c2d25dcce56cd57302512
Isaac-JenkinsRA <isaac.jenkins@riskaware.co.uk>
2020-05-07 15:09:32 +0100

    refs #21 Added run method for plots.
    Implemented run method. Calculated the concentration from the given config over time. Then performs a plot of this against time.

--- container/wellmixedrun.py
+++ container/wellmixedrun.py
@@ -5,31 +5,34 @@ from numpy import linspace
 from config.idmfconfig import IDMFConfig
 from base.settings import ComputationalSpace
 
+#################### Review imports before implementation
+from equation import WellMixed
+from analysis import PointPlot
+
 
 class WellMixedRun:
 
     def __init__(self, settings: IDMFConfig):
-
         self._settings = settings
-        self.prepare()
         self.build_parameter_space()
-    
+        self.run(settings)
+        # self.prepare()
+
     def build_parameter_space(self):
-        self._time= linspace(0.0,
-                             self.settings.total_time,
-                             self.settings.time_samples)
+        self._time = linspace(0.0,
+                              self.settings.total_time,
+                              self.settings.time_samples)
         self._space = (self.settings.time_samples)
-    
+
     def prepare(self):
-        restrict = { 
+        restrict = {
             "modes": self.settings.release_type,
             "models": self.settings.dispersion_model
         }
 
         space = ComputationalSpace(self.settings, restrict)
         data = zeros(shape=(*space.shape, *self._space))
-   
-   
+
     @property
     def settings(self):
         return self._settings
@@ -37,3 +40,10 @@ class WellMixedRun:
     @property
     def time(self):
         return self._time
+
+    ############################## To be reviewed before implementation
+    def run(self, settings: IDMFConfig):
+        wm = WellMixed(settings)
+        conc = wm(self._time)
+        pp = PointPlot(settings)
+        pp(conc)
