c00d93d667e7007a45f45b08826c6a2b3f2dbbf5
Isaac-JenkinsRA <isaac.jenkins@riskaware.co.uk>
2020-05-07 15:13:41 +0100

    refs #39 Refactoring Changes.
    Now calculates the time array within the constructor and additionally no longer takes in a save_dir parameter, instead gets this information from the settings object.

--- analysis/pointplot.py
+++ analysis/pointplot.py
@@ -4,6 +4,7 @@ from config.idmfconfig import Point
 from typing import List
 
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 class PointPlot:
@@ -13,29 +14,29 @@ class PointPlot:
 
         self.sources = getattr(
             self.settings.modes, self.settings.release_type).sources
+        self.t_array = np.linspace(
+            0, settings.total_time, settings.time_samples)
 
     def __call__(self,
                  concentrations: List[float],
-                 time_array: List[float],
-                 save_dir,
                  point: Point = None):
 
-        self.plot(concentrations, time_array)
-        self.save_fig(save_dir, point)
+        self.plot(concentrations)
+        self.save_fig(point)
 
-    def plot(self, concentrations: List[float], time_array: List[float]):
+    def plot(self, concentrations: List[float]):
         title = self.make_title()
 
         plt.title(title)
         plt.ylabel(f"Concentration ({self.settings.concentration_units})")
         plt.xlabel(f"time ({self.settings.time_units})")
 
-        plot = plt.plot(time_array, concentrations)
+        plot = plt.plot(self.t_array, concentrations)
 
         return plot
 
-    def save_fig(self, save_dir, point: Point = None):
-        name = f"{save_dir}/{self.settings.dispersion_model.capitalize()} " \
+    def save_fig(self, point: Point = None):
+        name = f"{self.settings.output_dir}/{self.settings.dispersion_model.capitalize()} " \
                f"{self.settings.release_type.capitalize()} {len(self.sources)} Source(s)"
         if point:
             name += f" Point at {[point.x, point.y, point.z]}"
