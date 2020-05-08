f717faadd58bf95a9497f9024b934de4b9cb2700
James de Lisle <james.delisle@riskaware.co.uk>
2020-05-08 11:55:03 +0100

    ref #43 readded the output dir argument to the cli

--- idmf.py
+++ idmf.py
@@ -12,7 +12,8 @@ from base.settings import ComputationalSpace
 
 @click.command()
 @click.argument('config_file', type=click.Path(exists=True))
-def idmf(config_file):
+@click.argument('output_dir', type=click.Path(exists=True))
+def idmf(config_file, output_dir):
     """Run one or several config files and write all data to the given directory."""
     try:
         with ConfigFileParser() as cfp:
@@ -22,16 +23,16 @@ def idmf(config_file):
             ConfigFileParserValidationError) as e:
         sys.exit(e)
 
-    s.consistency_check()
-    p = ComputationalSpace(s, {"models": "eddy_diffusion"})
-    p.cout_summary()
+    # s.consistency_check()
+    # p = ComputationalSpace(s, {"models": "eddy_diffusion"})
+    # p.cout_summary()
 
 
-    # if s.dispersion_model == "well_mixed":
-    #     WellMixedRun(s)
+    if s.dispersion_model == "well_mixed":
+        r = WellMixedRun(s)
 
-    # if s.dispersion_model == "eddy_diffusion":
-    #     EddyDiffusionRun(s)
+    if s.dispersion_model == "eddy_diffusion":
+        EddyDiffusionRun(s)
 
 
 if __name__ == '__main__':
