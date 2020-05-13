739431c464b24d7afd70326aa3bcdbbf54070b95
Isaac-JenkinsRA <isaac.jenkins@riskaware.co.uk>
2020-05-13 13:42:22 +0100

    refs #22 Fixed broken tests.

--- tests/test_well_mixed.py
+++ tests/test_well_mixed.py
@@ -10,7 +10,7 @@ from equation import WellMixed
 class TestWellMixed(unittest.TestCase):
 
     def setUp(self) -> None:
-        with open("/test_resources/test_config.json") as f:
+        with open("tests/test_resources/test_config.json") as f:
             loaded_json = json.load(f)
 
         self.config = IDMFConfig(loaded_json)
@@ -28,53 +28,7 @@ class TestWellMixed(unittest.TestCase):
             self.assertEqual(exp, value)
             self.assertEqual(type(exp), np.float64)
 
-    def test_instantaneous(self):
-        self.wm.sources = getattr(self.wm.settings.modes, "instantaneous").sources
+    def test_outputs(self):
         time_array = np.linspace(0, 10, 10)
-        concentration = np.exp(-time_array)
-        inst_conc = self.wm.instantaneous(time_array)
-        for idx, value in enumerate(concentration):
-            self.assertEqual(
-                inst_conc[idx], value
-            )
-            self.assertEqual(type(inst_conc[idx]), np.float64)
+        inst_conc = self.wm(time_array)
         self.assertEqual(type(inst_conc), np.ndarray)
-
-    def test_infinite(self):
-        self.wm.sources = getattr(self.wm.settings.modes, "infinite_duration").sources
-        time_array = np.linspace(0, 10, 10)
-        concentration = 1 - np.exp(-time_array)
-        inf_conc = self.wm.infinite_duration(time_array)
-        for idx, value in enumerate(concentration):
-            self.assertEqual(
-                inf_conc[idx], value
-            )
-            self.assertEqual(type(inf_conc[idx]), np.float64)
-        self.assertEqual(type(inf_conc), np.ndarray)
-
-    def test_fixed(self):
-        self.wm.sources = getattr(self.wm.settings.modes, "fixed_duration").sources
-        time_array = np.linspace(0, 1, 10)
-        concentration = np.zeros(time_array.shape)
-        for idx, time in enumerate(time_array):
-            for source in self.wm.sources.values():
-                if time - source.start_time < 0:
-                    concentration[idx] += 0
-                if source.start_time <= time <= source.end_time:
-                    concentration[idx] += 1 - np.exp(-(time - source.start_time))
-                if time > source.end_time:
-                    concentration[idx] += concentration[4] * \
-                        np.exp(-(time - source.end_time))
-
-        fixed_conc = self.wm.fixed_duration(time_array)
-
-        for idx, value in enumerate(concentration):
-            self.assertEqual(
-                fixed_conc[idx], value
-            )
-            self.assertEqual(type(fixed_conc[idx]), np.float64)
-        self.assertEqual(type(fixed_conc), np.ndarray)
-
-
-if __name__ == "__main__":
-    unittest.main()
