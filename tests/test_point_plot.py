4be77917e6d7b2fce6cf4405dd99794db50d6826
Isaac-JenkinsRA <isaac.jenkins@riskaware.co.uk>
2020-05-13 12:26:38 +0100

    refs #40 Fixed broken tests.

--- tests/test_point_plot.py
+++ tests/test_point_plot.py
@@ -6,34 +6,33 @@ import numpy as np
 from matplotlib.lines import Line2D
 
 from config import IDMFConfig
-from analysis import PointPlot
+from plot import PointPlot
 
 
 class TestPointPlot(unittest.TestCase):
 
     def setUp(self) -> None:
 
-        with open("test_resources/test_config.json") as f:
+        with open("tests/test_resources/test_config.json") as f:
             loaded_json = json.load(f)
 
         self.config = IDMFConfig(loaded_json)
         self.config.consistency_check()
 
-        self.pp = PointPlot(self.config)
+        self.pp = PointPlot(self.config, "tests/test_resources")
 
         self.time_array = np.linspace(0, 10, 10)
         self.conc = np.linspace(0, 10, 10)
-        self.save_dir = "test_resources"
         self.points = self.config.models.eddy_diffusion.monitor_locations.points
 
     def tearDown(self) -> None:
-        resource_list = os.listdir("test_resources")
+        resource_list = os.listdir("tests/test_resources")
         for file in resource_list:
             if file.endswith(".pdf"):
-                os.remove(f"test_resources/{file}")
+                os.remove(f"tests/test_resources/{file}")
 
     def test_plot(self):
-        plots = self.pp.plot(self.conc, self.time_array)
+        plots = self.pp.plot(self.conc)
 
         for plot in plots:
             self.assertEqual(type(plot), Line2D)
