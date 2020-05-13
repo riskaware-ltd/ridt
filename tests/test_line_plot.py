effd8c92618446978f75d811e7779e19bc14fc93
Isaac-JenkinsRA <isaac.jenkins@riskaware.co.uk>
2020-05-13 12:31:31 +0100

    refs #41 Fixed broken tests.

--- tests/test_line_plot.py
+++ tests/test_line_plot.py
@@ -6,31 +6,30 @@ import numpy as np
 from matplotlib.lines import Line2D
 
 from config import IDMFConfig
-from analysis import LinePlot
+from plot import LinePlot
 
 
-class TestPointPlot(unittest.TestCase):
+class TestLinePlot(unittest.TestCase):
 
     def setUp(self) -> None:
 
-        with open("test_resources/test_config.json") as f:
+        with open("tests/test_resources/test_config.json") as f:
             loaded_json = json.load(f)
 
         self.config = IDMFConfig(loaded_json)
         self.config.consistency_check()
 
-        self.lp = LinePlot(self.config)
+        self.lp = LinePlot(self.config, "tests/test_resources")
 
         self.time_array = np.linspace(0, 10, 10)
         self.conc = np.linspace(0, 10, 10)
-        self.save_dir = "test_resources"
         self.lines = self.config.models.eddy_diffusion.monitor_locations.lines
 
     def tearDown(self) -> None:
-        resource_list = os.listdir("test_resources")
+        resource_list = os.listdir("tests/test_resources")
         for file in resource_list:
             if file.endswith(".pdf"):
-                os.remove(f"test_resources/{file}")
+                os.remove(f"tests/test_resources/{file}")
 
     def test_plot(self):
         for line in self.lines.values():
