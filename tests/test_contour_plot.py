167ed9955470e3f27eced0704b9bea1b3f42b465
Isaac-JenkinsRA <isaac.jenkins@riskaware.co.uk>
2020-05-13 12:30:55 +0100

    refs #42 Fixed Broken Tests.

--- tests/test_contour_plot.py
+++ tests/test_contour_plot.py
@@ -6,32 +6,31 @@ import numpy as np
 from matplotlib.contour import QuadContourSet
 
 from config import IDMFConfig
-from analysis import ContourPlot
+from plot import ContourPlot
 
 
-class TestPointPlot(unittest.TestCase):
+class TestContourPlot(unittest.TestCase):
 
     def setUp(self) -> None:
 
-        with open("test_resources/test_config.json") as f:
+        with open("tests/test_resources/test_config.json") as f:
             loaded_json = json.load(f)
 
         self.config = IDMFConfig(loaded_json)
         self.config.consistency_check()
 
-        self.cp = ContourPlot(self.config)
+        self.cp = ContourPlot(self.config, "tests/test_resources")
 
         self.time_array = np.linspace(0, 10, 10)
         self.x_array = np.linspace(0, 10, 10)
         self.conc, _ = np.meshgrid(self.x_array, self.x_array)
-        self.save_dir = "test_resources"
         self.planes = self.config.models.eddy_diffusion.monitor_locations.planes
 
     def tearDown(self) -> None:
-        resource_list = os.listdir("test_resources")
+        resource_list = os.listdir("tests/test_resources")
         for file in resource_list:
             if file.endswith(".pdf"):
-                os.remove(f"test_resources/{file}")
+                os.remove(f"tests/test_resources/{file}")
 
     def test_plot(self):
         for plane in self.planes.values():
