585a3662974fa5b3480c56986bb1bbc80c50d065
Isaac-JenkinsRA <isaac.jenkins@riskaware.co.uk>
2020-05-16 15:59:20 +0100

    refs #29 Added DocStrings.

--- tests/test_eddy_diffusion.py
+++ tests/test_eddy_diffusion.py
@@ -10,11 +10,15 @@ from equation import EddyDiffusion
 class TestEddyDiffusion(unittest.TestCase):
 
     def setUp(self) -> None:
+
+        """setUp method which instantiates the :class:`~.IDMFConfig` class,
+        the :class:`~.EddyDiffusion` class, and creates some initial
+        variables."""
+
         with open("tests/test_resources/test_config.json") as f:
             loaded_json = json.load(f)
 
         self.config = IDMFConfig(loaded_json)
-        self.config.consistency_check()
 
         self.ed = EddyDiffusion(self.config)
 
@@ -24,18 +28,28 @@ class TestEddyDiffusion(unittest.TestCase):
         self.z_space = np.linspace(0, 10, 10)
 
     def test_outputs(self):
+
+        """Ensures the concentration is of type array"""
+
         concentration = self.ed(
             self.x_space, self.y_space, self.z_space, self.time_array
         )
         self.assertEqual(type(concentration), np.ndarray)
 
     def test__coeff(self):
+
+        """Tests the output of the coefficient under certain
+        conditions."""
+
         coeff = self.ed._EddyDiffusion__coeff(1)
         self.assertEqual(
             coeff, 1 / (8 * np.power(np.pi, 3/2) * np.exp(1))
         )
 
     def test_diffusion_coefficient(self):
+
+        """Ensures the correct output type of the coefficients."""
+
         self.ed.settings.models.eddy_diffusion.coefficient.calculation = "EXPLICIT"
         self.assertEqual(
             type(self.ed._EddyDiffusion__diffusion_coefficient()), float
@@ -46,6 +60,10 @@ class TestEddyDiffusion(unittest.TestCase):
         )
 
     def test_exp(self):
+
+        """"Makes sure that the exp function is
+        operating correctly."""
+
         bound = 1
         source_loc = 1
         t = 1
@@ -55,6 +73,10 @@ class TestEddyDiffusion(unittest.TestCase):
         self.assertEqual(type(rv), np.ndarray)
 
     def test_concentration(self):
+
+        """Makes sure that the concentration function outputs
+        the correct type."""
+
         self.ed.sources = getattr(self.ed.settings.modes, "fixed_duration").sources
         time = 1
         for source in self.ed.sources.values():
