198e205161c2753cfe7e72a6550a6c2f34fffbb9
Isaac-JenkinsRA <isaac.jenkins@riskaware.co.uk>
2020-05-16 16:06:17 +0100

    refs #57 Added tests and docs.

---
+++ tests/test_domain.py
@@ -0,0 +1,54 @@
+import unittest
+import json
+
+import numpy as np
+
+from config import IDMFConfig
+
+from container import Domain
+
+
+class TestDomain(unittest.TestCase):
+
+    """Unit tests for the :class:`~.Domain` class."""
+
+    def setUp(self) -> None:
+
+        """setUp method which instantiates the :class:`~.IDMFConfig` class,
+        and the :class:`~.Domain` class."""
+
+        with open("default/config.json") as f:
+            loaded_json = json.load(f)
+
+        self.config = IDMFConfig(loaded_json)
+        self.domain = Domain(self.config)
+
+    def test_point(self):
+
+        """Tests to make sure that the types of the points
+        are arrays."""
+
+        points = self.config.models.eddy_diffusion.monitor_locations.points
+        for point in points.values():
+            for array in self.domain.point(point):
+                self.assertEqual(type(array), np.ndarray)
+
+    def test_line(self):
+
+        """Tests to make sure that the types of the lines
+        are arrays."""
+
+        lines = self.config.models.eddy_diffusion.monitor_locations.lines
+        for line in lines.values():
+            for array in self.domain.line(line):
+                self.assertEqual(type(array), np.ndarray)
+
+    def test_plane(self):
+
+        """Tests to make sure that the types of the planes
+        are arrays."""
+
+        planes = self.config.models.eddy_diffusion.monitor_locations.planes
+        for plane in planes.values():
+            for array in self.domain.plane(plane):
+                self.assertEqual(type(array), np.ndarray)
