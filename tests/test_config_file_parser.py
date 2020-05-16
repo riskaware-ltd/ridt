1e45fe05ef8c08078eace67384b5d648ca38d423
Isaac-JenkinsRA <isaac.jenkins@riskaware.co.uk>
2020-05-16 15:59:52 +0100

    refs #32 Added DocStrings.

--- tests/test_config_file_parser.py
+++ tests/test_config_file_parser.py
@@ -9,11 +9,22 @@ from config.configfileparser import ConfigFileParserValidationError
 
 class TestConfigFileParser(unittest.TestCase):
 
+    """The unit tests for the :class:`~.ConfigFileParser` class."""
+
     def setUp(self) -> None:
+
+        """setUp method which instantiates the :class:`~.IDMFConfig` class,
+        and the :class:`~.ConfigFileParser` class."""
+
         self.idmf = IDMFConfig
         self.cfp = ConfigFileParser()
 
     def test_parse_file(self):
+
+        """Checks if the :class:`~.ConfigFileParserOSError` error
+        is passed when the path is unknown.
+        Checks to see if the :class:`~.ConfigFileParserJSONError` error
+        is passed when there isa decode error in the json file."""
 
         with self.assertRaises(ConfigFileParserOSError):
             self.cfp("random_path")
@@ -22,6 +33,9 @@ class TestConfigFileParser(unittest.TestCase):
             self.cfp("tests/test_resources/decode_error_config.json")
 
     def test_instantiate_settings(self):
+
+        """"Checks to make sure that the :class:`~.ConfigFileParserValidationError`
+        error passes when there is a missing input within the config file."""
 
         with self.assertRaises(ConfigFileParserValidationError):
             self.cfp("tests/test_resources/missing_input_config.json")
