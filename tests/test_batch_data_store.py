e03c90fd59bd16405a9b56218ddcfdeef0fb9c81
Isaac-JenkinsRA <isaac.jenkins@riskaware.co.uk>
2020-05-16 16:04:54 +0100

    refs #54 Added tests and docs.

---
+++ tests/test_batch_data_store.py
@@ -0,0 +1,36 @@
+import unittest
+import json
+
+from data import BatchDataStore
+from data import DataStore
+
+from config import IDMFConfig
+
+
+class TestBatchDataStore(unittest.TestCase):
+
+    "Unit tests for the :class:`~.BatchDataStore` class."""
+
+    def setUp(self) -> None:
+
+        """setUp method which instantiates the :class:`~.IDMFConfig` class,
+        and the :class:`~.BatchDataStore` class."""
+
+        with open("default/config.json") as f:
+            loaded_json = json.load(f)
+
+        self.config = IDMFConfig(loaded_json)
+
+        self.bds = BatchDataStore()
+
+    def test_add_run(self):
+
+        """Adds a run to the batch data store then checks
+        to make sure the new items match the type of
+        the ones added."""
+
+        self.bds.add_run(self.config)
+        self.assertEqual(type(self.bds.store), dict)
+        for key, val in self.bds.store.items():
+            self.assertEqual(type(key), IDMFConfig)
+            self.assertEqual(type(val), DataStore)
