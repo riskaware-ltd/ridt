0429f2b5b3071b11d560d3e9ba7930693cd7df2d
Isaac-JenkinsRA <isaac.jenkins@riskaware.co.uk>
2020-05-16 15:59:08 +0100

    refs #22 Added DocStrings.

--- tests/test_well_mixed.py
+++ tests/test_well_mixed.py
@@ -9,16 +9,25 @@ from equation import WellMixed
 
 class TestWellMixed(unittest.TestCase):
 
+    "Unit tests for the :class:`~.WellMixed` class."""
+
     def setUp(self) -> None:
+
+        """setUp method which instantiates the :class:`~.IDMFConfig` class,
+        the :class:`~.WellMixed` class, and creates some initial
+        variables."""
+
         with open("tests/test_resources/test_config.json") as f:
             loaded_json = json.load(f)
 
         self.config = IDMFConfig(loaded_json)
-        self.config.consistency_check()
 
         self.wm = WellMixed(self.config)
 
     def test_concentration(self):
+
+        """Makes sure that the concentration function outputs
+        the correct type."""
 
         for time in range(10):
             exp = self.wm.concentration(time)
@@ -29,6 +38,10 @@ class TestWellMixed(unittest.TestCase):
             self.assertEqual(type(exp), np.float64)
 
     def test_outputs(self):
+
+        """Ensures that the concentration output type
+        is ndarray."""
+
         time_array = np.linspace(0, 10, 10)
         inst_conc = self.wm(time_array)
         self.assertEqual(type(inst_conc), np.ndarray)
