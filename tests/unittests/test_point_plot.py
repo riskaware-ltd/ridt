204dfc315078ed5fa6cb62ef83b03c4a648562dc
Isaac-JenkinsRA <isaac.jenkins@riskaware.co.uk>
2020-05-20 09:07:26 +0100

    refs #40 Moved File.

---
+++ tests/unittests/test_point_plot.py
@@ -0,0 +1,64 @@
+import unittest
+import json
+import os
+
+import numpy as np
+from matplotlib.lines import Line2D
+
+from config import IDMFConfig
+from plot import PointPlot
+
+
+class TestPointPlot(unittest.TestCase):
+
+    "Unit tests for the :class:`~.PointPlot` class."""
+
+    def setUp(self) -> None:
+
+        """setUp method which instantiates the :class:`~.IDMFConfig` class,
+        the :class:`~.PointPlot` class, and creates some initial
+        variables."""
+
+        with open("tests/unittests/test_resources/test_config.json") as f:
+            loaded_json = json.load(f)
+
+        self.config = IDMFConfig(loaded_json)
+        self.config.consistency_check()
+
+        self.pp = PointPlot(self.config, "tests/unittests/test_resources")
+
+        self.time_array = np.linspace(0, 10, 10)
+        self.conc = np.linspace(0, 10, 10)
+        self.points = self.config.models.eddy_diffusion.monitor_locations.points
+
+    def tearDown(self) -> None:
+
+        """tearDown method which removes and changes made in the
+        tests."""
+
+        resource_list = os.listdir("tests/unittests/test_resources")
+        for file in resource_list:
+            if file.endswith(".pdf"):
+                os.remove(f"tests/unittests/test_resources/{file}")
+
+    def test_plot(self):
+
+        """tests the plots to make sure that is plot is
+        of type :class:`~.Line2D.`"""
+
+        plots = self.pp.plot(self.conc)
+
+        for plot in plots:
+            self.assertEqual(type(plot), Line2D)
+
+    def test_make_title(self):
+
+        """tests to make sure that the title
+        is of type :obj:`str`."""
+
+        title = self.pp.make_title()
+        self.assertEqual(type(title), str)
+
+
+if __name__ == "__main__":
+    unittest.main()
