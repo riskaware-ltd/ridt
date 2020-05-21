dcae17e31b9f35621def067d2af3a040bdad584c
Isaac-JenkinsRA <isaac.jenkins@riskaware.co.uk>
2020-05-21 10:16:32 +0100

    refs #71 First addition of ST08.

---
+++ tests/systemtests/test_st08.py
@@ -0,0 +1,38 @@
+import unittest
+
+from config import ConfigFileParser
+
+
+class ST08(unittest.TestCase):
+
+    """System Test 08. Test that the system can
+       parse the options relating to the number of
+       image sources and allowable error for the
+       Eddy diffusion model from the JSON file."""
+
+    def setUp(self) -> None:
+
+        """setUp method that instantiates the
+          :class:`~.IDMFConfig` class."""
+
+        with ConfigFileParser("default/config.json") as cfp:
+            self.c = cfp
+
+    def test_verify(self):
+
+        """Verify that the :class:`~.EddyDiffusion` class
+        contains the correct Image attributes."""
+
+        self.assertEqual(
+            hasattr(self.c.models.eddy_diffusion, "images"), True
+        )
+        self.assertEqual(
+            hasattr(self.c.models.eddy_diffusion.images, "quantity"), True
+        )
+        self.assertEqual(
+            hasattr(self.c.models.eddy_diffusion.images, "max_error"), True
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
