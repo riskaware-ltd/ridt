163aaed79e4e646581e00c701b03179fab1ed972
James de Lisle <james.delisle@riskaware.co.uk>
2020-05-24 09:47:23 +0100

    ref #47 #48 Added functions for returning real val.
    Can now get real vaules from indices and ids with new functions.

--- container/domain.py
+++ container/domain.py
@@ -1,3 +1,5 @@
+from typing import Tuple
+
 from numpy import linspace
 from numpy import meshgrid
 
@@ -38,10 +40,10 @@ class Domain:
     def time(self):
         return self._time
     
-    def point(self, point: Point):
+    def points(self, point: Point):
         return meshgrid(point.x, point.y, point.z)
 
-    def line(self, line: Line):
+    def lines(self, line: Line):
         if line.parallel_axis == "x":
             return meshgrid(self.x, line.point.y, line.point.z)
         elif line.parallel_axis == "y":
@@ -49,10 +51,51 @@ class Domain:
         else:
             return meshgrid(line.point.x, line.point.y, self.z)
 
-    def plane(self, plane: Plane):
+    def planes(self, plane: Plane):
         if plane.axis == "xy":
             return meshgrid(self.x, self.y, plane.distance)
         elif plane.axis == "yz":
             return  meshgrid(plane.distance, self.y, self.z)
         else:
             return meshgrid(self.x, plane.distance, self.z)
+    
+    def domain(self, *args, **kwargs):
+        return self.full
+    
+    def values(self, geometry: str, id: str, index: Tuple[int]):
+        if len(index) == 1:
+            return self.point_values(id, index) 
+        elif len(index) == 2:
+            return self.line_values(id, index) 
+        elif len(index) == 3:
+            return self.plane_values(id, index) 
+        elif len(index) == 4:
+            return self.domain_values(id, index) 
+        else:
+            # TODO Custom exception.
+            raise ValueError("Not a valid number of dimensions.")
+
+    def point_values(self, id: str, index: Tuple[int]):
+        point = self.set.models.eddy_diffusion.monitor_locations.points[id]
+        return self.time[index[0]], point.x, point.y, point.z
+        
+    def line_values(self, id: str, index: Tuple[int]):
+        line = self.set.models.eddy_diffusion.monitor_locations.lines[id]
+        if line.parallel_axis == "x":
+            return self.time[index[0]], self.x[index[1]], line.point.y, line.point.z
+        elif line.parallel_axis == "y":
+            return self.time[index[0]], line.point.x, self.y[index[1]], line.point.z
+        else:
+            return self.time[index[0]], line.point.x, line.point.y, self.z[index[1]] 
+        
+    def plane_values(self, id: str, index: Tuple[int]):
+        plane = self.set.models.eddy_diffusion.monitor_locations.planes[id]
+        if plane.axis == "xy":
+            return self.time[index[0]], self.x[index[1]], self.y[index[2]], plane.distance
+        elif plane.axis == "yz":
+            return self.time[index[0]], plane.distance, self.y[index[1]], self.z[index[2]]
+        else:
+            return self.time[index[0]], self.x[index[1]], plane.distance, self.z[index[2]] 
+    
+    def domain_values(self, id: str, index: Tuple[int]):
+        return self.time[index[0]], self.x[index[1]], self.y[index[2]], self.z[index[3]] 
