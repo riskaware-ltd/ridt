ad00de45d2d15c6980e8fdf574c636c2dbb4817c
James de Lisle <james.delisle@riskaware.co.uk>
2020-05-30 10:23:30 +0100

    ref #118 Docstrings added.

--- analysis/batchdatastoreanalyser.py
+++ analysis/batchdatastoreanalyser.py
@@ -25,11 +25,35 @@ BF = '{l_bar}{bar:30}{r_bar}{bar:-10b}'
 
 
 class BatchDataStoreAnalyser:
+    """Batch data store analyser class.
 
-    quantities = [
-        "concentration",
-        "exposure"
-    ]
+    Attributes
+    ----------
+    settings : :class:`~.RIDTConfig`
+        The settings for the run in question.
+
+    space : :class:`~.ComputationalSpace`
+        The :class:`~.ComputationalSpace` instance corresponding to the
+        :attr:`settings` attribute.
+    
+    outdir: :obj:`str`
+        The path to the output directory for the run.
+    
+    quantity: :obj:`str`
+        The string id for the quantity stored in the data  store.
+
+    data_store : :class:`~.BatchDataStore`
+        The batch data store to be analysed.
+    
+    dir_agent: :class:`~.DirectoryAgent`
+        The  :class:`~.DirectoryAgent` instance for this run.
+    
+    results : :obj:`dict`
+        A dictionary of :class:`~.DataStoreAnalyser` instances for each
+        :class:`~.RIDTConfig` object in :attr:`space`.
+
+    """
+
 
     def __new__(cls, *args, **kwargs):
         instance = super(BatchDataStoreAnalyser, cls).__new__(cls)
@@ -42,6 +66,23 @@ class BatchDataStoreAnalyser:
                  space: ComputationalSpace,
                  outdir: str,
                  quantity: str):
+        """The :class:`~.BatchDataStoreAnalyser` class.
+
+        Parameters
+        ----------
+        settings : :class:`~.RIDTConfig`
+            The settings for the run in question.
+        data_store : :class:`~.BatchDataStore`
+            The batch data store to be analysed.
+        space : :class:`~.ComputationalSpace`
+            The :class:`~.ComputationalSpace` instance corresponding to the
+            :attr:`settings` attribute.
+        outdir : :obj:`str`
+            The path to the output directory for the run.
+        quantity : :obj:`str`
+            The string id for the quantity stored in the data  store.
+
+        """
         self.settings = settings
         self.space = space
         self.outdir = outdir
@@ -53,6 +94,16 @@ class BatchDataStoreAnalyser:
         self.analyse()
     
     def analyse(self):
+        """The method that loops through the :class:`~.DataStore` instances
+
+        and calls the :class:`~.DataStoreAnalyser` and :class:`~.ResultsWriter`
+        classes on them.
+
+        Returns
+        -------
+        None
+    
+        """
         if self.space.zero:
             store = self.data_store[self.settings]
             result = DataStoreAnalyser(self.settings, store, self.quantity)
