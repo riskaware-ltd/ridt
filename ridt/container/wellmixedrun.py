c9555b07c7a5a2701fc4995e418cac7ec7783ff3
James de Lisle <james.delisle@riskaware.co.uk>
2020-07-02 18:34:05 +0100

    #27 Docstrings Added

--- ridt/container/wellmixedrun.py
+++ ridt/container/wellmixedrun.py
@@ -25,33 +25,94 @@ BF = '{l_bar}{bar:30}{r_bar}{bar:-10b}'
 
 
 class WellMixedRun:
+    """The class which orchestrates an Well Mixed model run.
 
+    Attributes
+    ----------
+    setting : :class:`~.RIDTConfig`
+        The settings for the run in question.
+
+    data_store : :class:`~.BatchDataStore`
+        The batch run data store for concentration values.
+
+    outdir: :obj:`str`
+        The path to the output directory for the run.
+    
+    space : :class:`~.ComputationalSpace`
+        The :class:`~.ComputationalSpace` instance corresponding to the
+        :attr:`settings` attribute.
+    
+    exposure_store : :obj:`Union`[:class:`~.BatchDataStore`, None]
+        The batch run data store for exposure values.
+    
+    """
     def __init__(self, settings: RIDTConfig, output_dir: str):
-        print("Preparing Well Mixed run... ")
-        self._settings = settings
-        self._output_dir = output_dir
+        """The constructor for the :class:`EddyDiffusion` class.
+
+        Parameters
+        ----------
+        settings : :class:`~.RIDTConfig`
+            The settings for the run in question.
+
+        outdir : :obj:`str`
+            The path to the output directory for the run.
+
+        """
+        self.settings = settings
+        self.outdir = output_dir
         self.data_store = BatchDataStore()
         self.exposure_store = None
         self.space = self.prepare()
-        print("Evaluating model over domain... ")
         self.evaluate()
-        print("Computing exposure...")
         self.compute_exposure()
-        print("Writing data to disk... ")
         self.write()
-        print("Producing plots... ")
         self.plot()
         print("\n")
 
-    def prepare(self):
+    def prepare(self) -> ComputationalSpace:
+        """Instantiates :class:`~.ComputationalSpace` instance.
+
+        Generates a :class:`~.ComputationalSpace` instance from
+        :attr:`settings`. 
+
+        Returns
+        -------
+        :class:`~.ComputationalSpace`
+            The Computational Space created from :attr:`settings`. 
+        """
+        print("Preparing Well Mixed run... ")
         restrict = {"models": "well_mixed"}
         return ComputationalSpace(self.settings, restrict)
     
-    def evaluate(self):
-        for setting in tqdm(self.space.space, bar_format=BF):
+    def evaluate(self) -> None:
+        """Loops over all elements in :attr:`space` and evaluates the model.
+
+        Returns
+        -------
+        None
+
+        """
+        print("Evaluating model over domain... ")
+        for setting in self.space.space:
+            count = f"{self.space.linear_index(setting) + 1}/{len(self.space)}"
+            print(f"Evaluating computational space element {count}")
             self.run(setting)
 
-    def run(self, setting: RIDTConfig):
+    def run(self, setting: RIDTConfig) -> None:
+        """Evaluates the model for a set of parameters.
+    
+        Writes output to :attr:`data_store`.
+
+        Parameters
+        ----------
+        settings : :class:`~.RIDTConfig`
+            The settings for the run in question.
+        
+        Returns
+        -------
+        None
+
+        """
         self.data_store.add_run(setting)
         domain = Domain(setting)
         solver = WellMixed(setting)
@@ -59,20 +120,61 @@ class WellMixedRun:
         self.data_store[setting].add("points", "well_mixed", output)
     
     def compute_exposure(self):
-        self.exposure_store = Exposure(self.settings, self.data_store)
+        """Computes the exposure from the concentration data.
 
-    def write(self):
-        BatchDataStoreWriter(self.settings, self.data_store, self.space, self.output_dir, "concentration")
-        BatchDataStoreWriter(self.settings, self.exposure_store, self.space, self.output_dir, "exposure")
+        Saves the result to a new data store :attr:`exposure_store`
 
-    def plot(self):
-        BatchDataStorePlotter(self.settings, self.data_store, self.space, self.output_dir, "concentration",)
-        BatchDataStorePlotter(self.settings, self.exposure_store, self.space, self.output_dir, "exposure")
+        Returns
+        -------
+        None
 
-    @property
-    def settings(self):
-        return self._settings
+        """
+        if self.settings.compute_exposure:
+            print("Computing exposure...")
+            self.exposure_store = Exposure(self.settings, self.data_store)
 
-    @property
-    def output_dir(self):
-        return self._output_dir
+    def args(self, data_store: BatchDataStore, quantity: str) -> tuple:
+        """A helper function that returns a tuple of some values.
+
+        Parameters
+        ----------
+
+        data_store : :class:`~.BatchDataStore`
+            A batch data store.
+
+        quantity : :obj:`str`
+            The name of the quantity in the data store.
+
+        Returns
+        -------
+        :obj:`tuple`
+            The various quantites as a tuple.
+
+        """
+        return self.settings, data_store, self.space, self.outdir, quantity
+
+    def write(self) -> None:
+        """Writes all data stores to disk.
+
+        Returns
+        -------
+        None
+
+        """
+        print("\nWriting data to disk... ")
+        BatchDataStoreWriter(*self.args(self.data_store, "concentration"))
+        if self.settings.compute_exposure:
+            BatchDataStoreWriter(*self.args(self.exposure_store, "exposure"))
+
+    def plot(self) -> None:
+        """Plots all requested data and writes it to disk.
+
+        Returns
+        -------
+        None
+
+        """
+        print("\nProducing plots... ")
+        BatchDataStorePlotter(*self.args(self.data_store, "concentration"))
+        if self.settings.compute_exposure:
+            BatchDataStorePlotter(*self.args(self.exposure_store, "exposure"))
