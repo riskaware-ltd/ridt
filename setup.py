2a582ed69bfc1f45faaa15badbdb80ef6f3ede21
James de Lisle <james.delisle@riskaware.co.uk>
2020-07-02 19:13:19 +0100

    #133 New meta info added.

--- setup.py
+++ setup.py
@@ -1,10 +1,23 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='ridt',
     version='1.0',
+    author="Riskaware Ltd",
     packages=find_packages(),
+    description="Rapid Indoor Diffusion Tool",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/riskaware-ltd/ridt",
     include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
     install_requires=[
         'click',
         'numpy',
